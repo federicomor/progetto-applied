a1b05b9cc54b6c06aa5ab97708a26814246ce5cb
Federico Angelo Mor <federangelomor@gmail.com>
2023-05-31 17:49:14 +0200

    sperimentando col bot

--- game/bot/main_tg.jl
+++ game/bot/main_tg.jl
@@ -1,6 +1,50 @@
-using Telegram
+using Telegram, Telegram.API
 using ConfigEnv
 
 dotenv()
 
-Telegram.sendMessage(text = "Hello world")
+# sendMessage(text = "Hello world")
+
+# println(tg.getMe())
+
+# open("picture.png", "r") do io
+#     sendPhoto(photo = io)
+# end
+
+# run_bot() do msg
+#     message = get(msg, :message, nothing)
+#     message === nothing && return nothing
+#     text = get(message, :text, "")
+#     chat = get(message, :chat, nothing)
+#     chat === nothing && return nothing
+#     chat_id = get(chat, :id, nothing)
+#     chat_id === nothing && return nothing
+    
+#     if match(r"^[0-9 \.]+$", text) !== nothing
+#         x = parse.(Float64, split(text, " "))
+#         sendMessage(text = "your input squared is $(x.^2)")
+#     else
+#         sendMessage(text = "Unknown command")
+#     end
+# end
+
+run_bot() do msg
+    message = get(msg, :message, nothing)
+    message === nothing && return nothing
+    text = get(message, :text, "")
+    chat = get(message, :chat, nothing)
+    chat === nothing && return nothing
+    chat_id = get(chat, :id, nothing)
+    chat_id === nothing && return nothing
+    
+    if match(r"^[0-9 \.]+$", text) !== nothing
+        x = parse.(Float64, split(text, " "))
+        sendMessage(text = "Ricevuto, siamo operativi")
+        sendDice()
+
+        sendPoll(question="Which state do you want to play with",
+        		 options=["ESP","CKW","ENG"])
+    else
+        sendMessage(text = "Unknown command")
+    end
+end
