bfb3729ca45836640f20629d174cd9725672ae15
Federico Angelo Mor <federangelomor@gmail.com>
2023-06-24 17:39:08 +0200

    miglioramenti plot classifica

--- game/bot/visualize_score.jl
+++ game/bot/visualize_score.jl
@@ -2,8 +2,11 @@ using DataFrames
 using CSV
 using UnicodePlots
 # using Plots
+using Dates
 
 data = DataFrame(CSV.File("df.csv",stringtype=String))
+# data = data[isequal.(data.zdone,1),:]
+# select the done=1 players
 sort!(data,:score,rev=true)
 # ora il dataset è ordinato
 
@@ -37,12 +40,21 @@ println("done.")
 println("Writing scoreboard 2...")
 f = open("scoreboard_plot.md", "w")
 
-write(f,"# 🚩 Final Scoreboard\n\n")
-write(f,"```\n")
+write(f,"# 🚩 Live Scoreboard\n\n")
+write(f,"```R\n")
 
-P = barplot(data[:,:player_name],data[:,:score])
-savefig(p,"plot.md")
-for line in eachline("plot.md")
+t_end = Dates.Time(19,00,00)
+# t_now = Dates.Time(now())
+
+P = barplot(string.(data[:,:player_name]," ",1:size(data)[1] ), data[:,:score],
+	# width=:auto, # altrimenti tipo 10 si stringe, 100 si allarga
+	width = 20,
+	# così stretta che forse dal telefono si vede meglio
+	title="Game ends at 19:00!",
+	border=:ascii)
+
+savefig(P,"plot.txt")
+for line in eachline("plot.txt")
 	write(f,"$line\n")
 end
 
